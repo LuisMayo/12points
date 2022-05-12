78e22d7bb65b88c6425a912596aead4a8032b0d5
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2022-05-12 18:16:13 +0200

    Showing votes in rooms

--- src/app/pipes/voted-by.pipe.ts
+++ src/app/pipes/voted-by.pipe.ts
@@ -6,9 +6,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class VotedByPipe implements PipeTransform {
 
   transform(contestant, ...args: any[][]): string {
-    const users: any[] = args[0];
+    const users: any[] = args[0].filter(user => user.votes.find(vote => vote.contestant.id === contestant.id) != null);
     console.log(users);
-    return 'Voted by ' + users.slice(0, 2).map(u => u.username).join(', ') + (users.length > 2 ? ` and ${users.length - 2} more` : '');
+    const usersFiltered = users.slice(0, 15);
+    // Create the voted by text. If nobody voted it, it's an special text. Else we concat the use rnames + votes using the reduce function
+    const showField =  usersFiltered.length === 0 ? 'No votes' : usersFiltered.reduce((prev, user, index, arr) => {
+      // Is it the last name?
+      const chunkEnd = index === arr.length - 1 ? '' : ', ';
+      return `${prev}${user.username} (${user.votes.find(vote => vote.contestant.id === contestant.id).points})${chunkEnd}`;
+    }, 'Voted by ');
+    return showField;
   }
 
 }
